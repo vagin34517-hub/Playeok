cff370e2839381fac50f564062f0ee52bbdae461
vagin34517-hub <vagin34517@gmail.com>
2026-06-10 17:55:10 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// Playerok mini-app config
+// Поменяй API_BASE если перезапустишь serveo и получишь новый URL
+window.PLAYEROK_CONFIG = {
+  API_BASE: "https://bc3de2c0c2713cbd-66-90-105-35.serveousercontent.com",
+  TIMEOUT_MS: 9000
+};
